2636ac49137450eb60a391d8beccfc53dae4bf8c
probsharpsat <probsharpsat@gmail.com>
2019-04-19 12:37:50 +0530

    minor changes in flag definitions

--- bin/probsharpsat.py
+++ bin/probsharpsat.py
@@ -13,12 +13,13 @@ def main():
     parser.add_argument("-noPP", action='store_true', help="turn off preprocessing", dest='noPP')
     parser.add_argument("-noCC", action='store_true', help="turn off component caching", dest='noCC')
     parser.add_argument("-noIBCP", action='store_true', help="turn off implicit BCP", dest='noIBCP')
-    parser.add_argument("-pcc", action='store_true', help="use probablistic component caching (PCC)", dest='PCC')
-    parser.add_argument("-csvsads", action='store_true', help="use CSVSADS branching heuristic", dest='CSVSADS')
-    parser.add_argument("-pc", action='store_true', help="use polarity cache (PC)", dest='PC')
-    parser.add_argument("-is", help="use independent support (IS)", dest='IS', action='store_true')
-    parser.add_argument("-edr", help="use exponentially decaying randomness (EDR)", dest='EDR', action='store_true')
-    parser.add_argument("-lso ", type=int, default=0, help="use LSO heuristic after LSO decisions", dest='LSO')
+    parser.add_argument("-noPCC", action='store_true', help="turn off probablistic component caching (PCC)", dest='PCC')
+    parser.add_argument("-noCSVSADS", action='store_true', help="turn off CSVSADS branching heuristic", dest='CSVSADS')
+    parser.add_argument("-noPC", action='store_true', help="turn off polarity cache heuristic (PC)", dest='PC')
+    parser.add_argument("-noIS", help="turn off independent support heuristic (IS)", dest='IS', action='store_true')
+    parser.add_argument("-noLSO", help="turn off learn and start over heuristic (LSO)", dest='NOLSO', action='store_true')
+    parser.add_argument("-EDR", help="use exponentially decaying randomness (EDR)", dest='EDR', action='store_true')
+    parser.add_argument("-lso ", type=int, default=5000, help="use learn and start over heuristic after LSO decisions", dest='LSO')
     parser.add_argument("-delta ", type=float, default=0.05, help="the confidence parameter", dest='DELTA')
     parser.add_argument("-seed", type=int, default=1000, help="seed for randomness", dest='seed')
     args = parser.parse_args()
@@ -38,15 +39,15 @@ def main():
         cmd += "-noPP "
     if (args.q):
         cmd += "-q "
-    if (args.PCC):
+    if (not args.PCC):
         cmd += "-pcc "
-    if (args.CSVSADS):
+    if (not args.CSVSADS):
         cmd += "-act "
-    if (args.PC):
+    if (not args.PC):
         cmd += "-pol random "
     else:
         cmd += "-pol default "
-    if (args.LSO != 0):
+    if (not args.NOLSO):
         cmd += "-res " + str(args.LSO) + " "
     if (args.EDR):
         cmd += "-rand "  
@@ -54,7 +55,7 @@ def main():
     #setting the value of delta
     cmd += "-delta " + str(args.DELTA) + " "
     new_cmd = cmd
-    if (args.IS):
+    if (not args.IS):
         cmd += "-maxdec " + "1000000" + " " + "500" + " "   #number of decisions and number of conflicts
     
     #setting the range of clhash family (= m*64)
@@ -98,8 +99,8 @@ def main():
         cmd += "-m " + str(m) + " "
         if(mis_calculated):
             cmd += "-is mis.out "
-        else:
-            cmd += "-maxdec " + "1000000" + " " + "500" + " "
+        elif (not args.IS):
+            cmd += "-maxdec " + "1000000" + " " + "500" + " " #number of decisions and number of conflicts
         cmd += args.DIMACSCNF + " > outputfile 2>&1 "
         os.system(cmd)
         f = open("timeoutfile")
